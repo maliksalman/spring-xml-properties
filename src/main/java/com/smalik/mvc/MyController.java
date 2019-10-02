53065d208714a2c59949210f138c29efee4a16cc
Salman Malik <smalik@pivotal.io>
2019-10-02 16:34:42 -0400

    readjusting the output

--- src/main/java/com/smalik/mvc/MyController.java
+++ src/main/java/com/smalik/mvc/MyController.java
@@ -18,16 +18,22 @@ public class MyController {
     @Autowired
     private Environment environment;
 
-    @GetMapping("/env")
+    @GetMapping("/")
     public Map<String, Object> getEnvironment() {
         Map<String, Object> map = new HashMap<String, Object>();
+        map.put("environment", getEnvironmentPropertiesMap());
+        map.put("placeholder", bean);
+        return map;
+    }
+
+    private Map<String, String> getEnvironmentPropertiesMap() {
+        Map<String, String> map = new HashMap<String, String>();
         map.put("messageA", environment.getProperty("message.a", "java-default"));
         map.put("messageB", environment.getProperty("message.b", "java-default"));
         map.put("messageC", environment.getProperty("message.c", "java-default"));
         map.put("messageD", environment.getProperty("message.d", "java-default"));
         map.put("messageE", environment.getProperty("message.e", "java-default"));
         map.put("messageF", environment.getProperty("message.f", "java-default"));
-        map.put("bean", bean);
         return map;
     }
 }
